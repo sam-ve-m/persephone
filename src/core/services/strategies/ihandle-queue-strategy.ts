1960885b24f8b1ba40c7aa6d6d085ff60724fa24
IgorMagro <iggormagro8@gmail.com>
2021-05-19 16:19:24 -0300

    feat: add handle queue strategy

---
+++ src/core/services/strategies/ihandle-queue-strategy.ts
@@ -0,0 +1,3 @@
+export interface IHandleQueueStrategy {
+  handleMessage(): void;
+}
---
+++ src/services/strategies/user-handle-queue-strategy.ts
@@ -0,0 +1,7 @@
+import { IHandleQueueStrategy } from "@core/services/strategies/ihandle-queue-strategy";
+
+export class UserHandleQueueStrategy implements IHandleQueueStrategy {
+  public constructor() {}
+
+  public handleMessage(): void {}
+}
