62a5da4c2c7c92de9839efd385c1a4b06df390c4
IgorMagro <iggormagro8@gmail.com>
2021-05-19 14:41:48 -0300

    feat: add queue controller

---
+++ src/controllers/queue-controller.ts
@@ -0,0 +1,24 @@
+import { IQueueServiceDependencyFactory } from "@/core/services/factories";
+
+import env from "@root/env";
+import { POINT_CONVERSION_COMPRESSED } from "constants";
+
+import redis from "redis";
+
+export class QueueController {
+  public runQueueConsumerService(): void {
+    console.log("Running persephone");
+
+    //Call handleQueueMessages
+  }
+
+  // const client = redis.createClient({
+  //   host: env.redis_metadata.redis_host,
+  //   port: env.redis_metadata.redis_port,
+  // });
+
+  // client.send_command("AUTH", [
+  //   env.redis_metadata.redis_user,
+  //   env.redis_metadata.password,
+  // ]);
+}
