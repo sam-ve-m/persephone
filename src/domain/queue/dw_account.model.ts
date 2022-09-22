d6a85460ef72f899279c2c01791d8f9096aea0c5
Marco Sievers de Almeida <msa@lionx.com.br>
2022-09-22 05:26:04 -0300

    feat: remove dw account

--- src/domain/queue/dw_account.model.ts
+++
@@ -1,9 +0,0 @@
-import { model } from "mongoose";
-
-import { IDwAccountData } from "@domain/sphinx/dw_account.types";
-import DwAccountDataSchema from "@domain/sphinx/dw_account.schema";
-
-export const DwAccountDataModel = model<IDwAccountData>(
-    "dw_account",
-    DwAccountDataSchema
-);
